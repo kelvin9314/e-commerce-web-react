b2628c3eef587af5f66dee5591d045f93bf702f8
kelvin mok <kelvin9314@gmail.com>
2019-12-30 00:36:37 +0800

    add files for homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage'
+
 function App() {
   return (
     <div >
-      Hello word
+      <HomePage />
     </div>
   );
 }
