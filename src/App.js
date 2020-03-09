b1cc77c99899cfa4954fa468b97a23fe7f1f35d8
kelvin9314 <kelvin9314@gmail.com>
2020-03-09 22:10:11 +0800

    basic routering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
 import './App.css';
 
 import HomePage from './pages/homepage/';
 
-function App() {
+const HatsPage = () => {
   return (
     <div >
-      <HomePage />
+      <h1> hats page </h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
