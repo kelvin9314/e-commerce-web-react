138e292d705095d7abe9515fee76dd789761f5b9
kelvin9314 <kelvin9314@gmail.com>
2020-03-29 11:24:47 +0800

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import { Switch, Route, Link } from "react-router-dom";
 
 import './App.css';
 
-// import HomePage from './pages/homepage/';
+import HomePage from './pages/homepage/';
 
-const HomePage = props => {
-  console.log(props);
-  return (
-    <div>
-      {/* <Link to='/hats'>Hats</Link> */}  
-      <button onClick={() => props.history.push('/hats')}>Hats</button>
-      <h1> Home page</h1>
-    </div>
-  )
-}
+// const HomePage = props => {
+//   console.log(props);
+//   return (
+//     <div>
+//       {/* <Link to='/hats'>Hats</Link> */}  
+//       <button onClick={() => props.history.push('/hats')}>Hats</button>
+//       <h1> Home page</h1>
+//     </div>
+//   )
+// }
 
 const HatsPage = props => {
   console.log(props);
   return (
     <div > 
-      <h1> hats page </h1>
+      <h1> Hats page </h1>
       <Link to={`${props.match.url}/13`}>To hats 13</Link>
       <Link to={`${props.match.url}/17`}>To hats 17</Link>
       <Link to={`${props.match.url}/21`}>To hats 21</Link>
--- src/components/directory/index.js
+++ src/components/directory/index.js
@@ -11,8 +11,8 @@ const Directory = () => {
   return (
     <div className="directory-menu">
       {
-        sections.map( ({title, imageUrl, id, size} ) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        sections.map( ({id, ...otherSectionProps} ) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))
       }
     </div>
