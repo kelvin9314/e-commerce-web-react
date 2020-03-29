276818909a1c192a137c9578d1a901bd6720797d
kelvin9314 <kelvin9314@gmail.com>
2020-03-29 18:06:59 +0800

    feat: 72 header component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css'
 
 import HomePage from './pages/homepage'
 import ShopPage from './pages/shop'
+import Header from './components/header'
 
 function App () {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
