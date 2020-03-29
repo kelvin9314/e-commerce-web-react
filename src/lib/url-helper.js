b646702586b145aa93af44a8f917f081dfcb99a6
kelvin9314 <kelvin9314@gmail.com>
2020-03-29 22:26:30 +0800

    [env] change name

--- src/lib/url-helper.js
+++ src/lib/url-helper.js
@@ -1,1 +1,1 @@
-export const withGithubPageRoute = restOfRoute => (`/${process.env.REACT_APP_API_TOKEN}${restOfRoute}`)
+export const withGithubPageRoute = restOfRoute => (`/${process.env.REACT_APP_GITHUB_REPO}${restOfRoute}`)
