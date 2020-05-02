5b445dee3cde0df0db38f223eb2d57148c1dba01
kelvin9314 <kelvin9314@gmail.com>
2020-05-02 17:03:05 +0800

    selector Memoization

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -6,12 +6,13 @@ import { ReactComponent as ShoppingIcon } from '../../assets/icons/shopping-bag.
 import './cart-icon.scss'
 
 const CartIcon = () => {
+  const itemCount = useSelector((state) => state.cart.cartItems.reduce((acc, curr) => acc + curr.quantity, 0))
   const dispatch = useDispatch()
 
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount} </span>
     </div>
   )
 }
