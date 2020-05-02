061256d745421f3872200cff30c862cd0febe1b5
kelvin9314 <kelvin9314@gmail.com>
2020-05-03 00:00:18 +0800

    redux-persist setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import { persistStore } from 'redux-persist'
 
 import rootReducer from './root.reducer'
 
 // define a array is more scalable to add or remove other middleware in the future
 const middleware = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middleware))
+export const store = createStore(rootReducer, applyMiddleware(...middleware))
 
-export default store
+export const persistor = persistStore(store)
+
+export default { store, persistor }
